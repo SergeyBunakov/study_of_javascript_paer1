26f6c5b00405e0127ddc673cd0031fb474661e34
SergeyBunakov <bunakov.sergey@gmail.com>
2021-05-24 00:33:36 +0300

    _

--- lesson-29-asynchrony/task1/script.js
+++ lesson-29-asynchrony/task1/script.js
@@ -4,9 +4,98 @@ console.log('              ---------');
 
 console.log("Async/await");
 
+/*
+// === перший крок ===
+const buttonElem = document.querySelector('#button');
+
+buttonElem.addEventListener('click', () => console.log('Click Me'));
+
+let sum = 0;
+const startTime = new Date();
+console.log("Cycle started");
+for (let i = 0; i < 5e5; i++) {
+  sum += i;
+}
+const endTime = new Date();
+console.log(`Cycle took ${endTime - startTime}ms`);
+*/
+
+/*
+
+const addImage = (imgSrc) => {
+  const imgElem = document.createElement('img');
+  imgElem.setAttribute('alt', 'My Image');
+  imgElem.src = imgSrc;
+  const containerElem = document.querySelector('.page');
+  containerElem.append(imgElem);
+
+  const { width, height } = imgElem;
+
+  return { width, height };
+};
+
+const imgSrc = 'https://gromcode.s3.eu-central-1.amazonaws.com/front-end/html-css/lesson15/task1/big.jpeg';
+
+const { width, height } = addImage(imgSrc);
+
+const sizeElem = document.querySelector('.image-size');
+sizeElem.textContent = `${width} x ${height}`;
+*/
+
+/*
+// === другий крок ===
+const addImage = (imgSrc) => {
+  const imgElem = document.createElement('img');
+  imgElem.setAttribute('alt', 'My Image');
+  imgElem.src = imgSrc;
+  const containerElem = document.querySelector('.page');
+  containerElem.append(imgElem);
+
+  const onImageLoaded = () => {
+    const { width, height } = imgElem;
+    console.log( width, height);
+  }
+
+  imgElem.addEventListener('load', onImageLoaded);
+
+  const { width, height } = imgElem;
+
+  return { width, height };
+};
+
+const imgSrc = 'https://gromcode.s3.eu-central-1.amazonaws.com/front-end/html-css/lesson15/task1/big.jpeg';
+
+const { width, height } = addImage(imgSrc);
+
+const sizeElem = document.querySelector('.image-size');
+sizeElem.textContent = `${width} x ${height}`;
+*/
 
 
+// === третій крок ===
+const addImage = (imgSrc, callback) => {
+  const imgElem = document.createElement('img');
+  imgElem.setAttribute('alt', 'My Image');
+  imgElem.src = imgSrc;
+  const containerElem = document.querySelector('.page');
+  containerElem.append(imgElem);
 
+  const onImageLoaded = () => {
+    const { width, height } = imgElem;
+    callback ({ width, height });
+  }
+
+  imgElem.addEventListener('load', onImageLoaded);
+};
+
+const imgSrc = 'https://gromcode.s3.eu-central-1.amazonaws.com/front-end/html-css/lesson15/task1/big.jpeg';
+
+const onImageLoaded = ({ width, height }) => {
+  const sizeElem = document.querySelector('.image-size');
+  sizeElem.textContent = `${width} x ${height}`;
+}
+
+addImage(imgSrc, onImageLoaded);
 
 
 

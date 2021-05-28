54c3d8e09fbfd1df041b8396c20c881332a64852
SergeyBunakov <bunakov.sergey@gmail.com>
2021-05-28 08:42:54 +0300

    added async/await at task2

--- lesson-36-async_await/task1/script.js
+++ lesson-36-async_await/task1/script.js
@@ -2,6 +2,7 @@
 console.log('--- JavaScript. async/await. Task-1 ---');
 console.log('              ---------');
 
+/*
 async function func() {
   const promise = new Promise(resolve => {
     setTimeout(() => {
@@ -10,8 +11,49 @@ async function func() {
   })
   const result = await promise;
   console.log(result);
+  console.log('Hello');
   return 1;
 }
 
 const result = func();
 console.log("Hi!");
+*/
+
+/*
+const getUser = async userId => {
+  const response = await fetch(`https://api.github.com/users/${userId}`);
+  console.log(response);
+}
+getUser('facebook');
+*/
+
+/*
+const getUser = async userId => {
+  const response = await fetch(`https://api.github.com/users/${userId}`);
+  if (!response.ok) {
+    return null;
+  }
+  const userData = await  response.json();
+  // console.log(userData);
+  return userData;
+}
+getUser('facebook');
+*/
+
+const getUser = async userId => {
+  try {
+    const response = await fetch(`https://api.github.com/users/${userId}`);
+    if (!response.ok) {
+      return null;
+    }
+    const userData = await response.json();
+    return userData;
+  } catch (err) {
+    // console.log(err);
+    throw new Error('Failed to fetch user!');
+  }
+};
+
+getUser('facebook')
+    .then(userData => console.log(userData))
+    .catch(err => alert(err.message));
